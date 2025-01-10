b12203e985e1284736ceecd918f22d899533ca93
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2025-01-11 00:29:03 +0530

    added

--- client/src/page/LoginPage.jsx
+++ client/src/page/LoginPage.jsx
@@ -1,27 +1,32 @@
 import React, { useState } from 'react';
-import { 
-  Alert, 
-  Box, 
-  Typography, 
+import {
+  Alert,
+  Box,
+  Typography,
   TextField,
   IconButton,
   InputAdornment,
   useMediaQuery,
   useTheme
 } from '@mui/material';
-import { Mail, Lock, Visibility, VisibilityOff } from '@mui/icons-material';
+import { Mail, Lock, Visibility, VisibilityOff, MailOutline, LockOutlined } from '@mui/icons-material';
 import CustomButton from './../components/CustomButton';
 import img from '../assets/loginImage.jpg';
-import styles from '../styles/LoginForm.module.css'; 
+import styles from '../styles/LoginForm.module.css';
+import TextInput from '../components/TextInput';
+import axios from 'axios';
+import { login } from '../api/apiService';
+import AlertComponent from '../components/AlertComponent';
 
 const LoginPage = () => {
   const [alertMessage, setAlertMessage] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: '',
     password: ''
   });
-  
+
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
@@ -36,17 +41,34 @@ const LoginPage = () => {
     e.preventDefault();
     try {
       console.log('Form submitted:', formData);
-      setAlertMessage(null);
+      const response = await login(formData.email, formData.password);
+      setShowAlert(true);  
+      console.log(response.data);
     } catch (error) {
-      setAlertMessage('This is an info alert — check it out!');
+      setShowAlert(true);
+      // alert('This is an info alert — check it out!');
     }
   };
+  
 
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginCard}>
         <div className={styles.loginFormSection}>
           <div className={styles.welcomeText}>
+
+          {showAlert ? (
+            <AlertComponent 
+            message = {"User Login Successful"}
+            type = {"success"}
+            />
+          ) : (
+            <AlertComponent 
+              message = {"User Login Have An issue"}
+              type = {"error"}
+              />
+          )}
+
             <Typography className={styles.formTitle} variant="h4">
               Welcome Back!
             </Typography>
@@ -57,48 +79,29 @@ const LoginPage = () => {
 
           <form onSubmit={handleSubmit}>
             <div className={styles.formGroup}>
-              <TextField
-                fullWidth
-                label="Email"
+              <TextInput
+                label={"Email"}
                 name="email"
-                type="email"
+                type={"email"}
                 value={formData.email}
                 onChange={handleChange}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Mail size={20} />
-                    </InputAdornment>
-                  ),
-                }}
+                fullWidth
+                required
+                icon={MailOutline}
+                positionStart={"start"}
               />
             </div>
 
             <div className={styles.formGroup}>
-              <TextField
-                fullWidth
+              <TextInput
                 label="Password"
                 name="password"
-                type={showPassword ? 'text' : 'password'}
+                type="password"
+                icon={LockOutlined}
                 value={formData.password}
                 onChange={handleChange}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Lock size={20} />
-                    </InputAdornment>
-                  ),
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        onClick={() => setShowPassword(!showPassword)}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
+                showPassword={showPassword}
+                setShowPassword={setShowPassword}
               />
             </div>
 
